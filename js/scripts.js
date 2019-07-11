0155a55ab9220b454368f2ac0131efaab956f900
Mateusz Delegacz <deli@Mateuszs-MacBook-Pro.local>
2019-07-11 21:48:51 +0100

    1.0 Css added, layout added, jquery ux added

--- js/scripts.js
+++ js/scripts.js
@@ -3,75 +3,71 @@ var listOfIngridients = [{ id : 1, ingridientName : "Brazilian", procentage : 0,
                          { id : 3, ingridientName : "Macadamia", procentage : 0, isAdded : false}, 
                          { id : 4, ingridientName : "Hazel", procentage : 0, isAdded : false}];
 
-function add1(listOfIngridients){
+function add1(){
     if (this.listOfIngridients[0].isAdded) {
-        console.log(this.listOfIngridients[0].isAdded);
     } else {
        this.listOfIngridients[0].isAdded = true;
-       console.log(this.listOfIngridients[0].isAdded);
+       $('#ing1 label').addClass('added');
     } 
 };
-function remove1(listOfIngridients) {
-    console.log('- button pressed');
+function remove1() {
     if (this.listOfIngridients[0].isAdded) {
         this.listOfIngridients[0].isAdded = false;
+        $('#ing1 label').removeClass('added');
     } else {
     }
+    
 }
-function add2(listOfIngridients){
+function add2(){
     if (this.listOfIngridients[1].isAdded) {
-        console.log(this.listOfIngridients[1].isAdded);
     } else {
        this.listOfIngridients[1].isAdded = true;
-       console.log(this.listOfIngridients[1].isAdded);
+       $('#ing2 label').addClass('added');
     } 
 };
-function remove2(listOfIngridients) {
-    console.log('- button pressed');
+function remove2() {
     if (this.listOfIngridients[1].isAdded) {
         this.listOfIngridients[1].isAdded = false;
+        $('#ing2 label').removeClass('added');
     } else {
     }
 }
-function add3(listOfIngridients){
+function add3(){
     if (this.listOfIngridients[2].isAdded) {
-        console.log(this.listOfIngridients[2].isAdded);
     } else {
        this.listOfIngridients[2].isAdded = true;
-       console.log(this.listOfIngridients[2].isAdded);
+       $('#ing3 label').addClass('added');
     } 
 };
-function remove3(listOfIngridients) {
-    console.log('- button pressed');
+function remove3() {
     if (this.listOfIngridients[2].isAdded) {
         this.listOfIngridients[2].isAdded = false;
+        $('#ing3 label').removeClass('added');
     } else {
     }
 }
-function add4(listOfIngridients){
+function add4(){
     if (this.listOfIngridients[3].isAdded) {
-        console.log(this.listOfIngridients[3].isAdded);
     } else {
        this.listOfIngridients[3].isAdded = true;
-       console.log(this.listOfIngridients[3].isAdded);
+       $('#ing4 label').addClass('added');
     } 
 };
-function remove4(listOfIngridients) {
-    console.log('- button pressed');
+function remove4() {
     if (this.listOfIngridients[3].isAdded) {
         this.listOfIngridients[3].isAdded = false;
+        $('#ing4 label').removeClass('added');
     } else {
-    }
+    }    
 }
-console.log(listOfIngridients);
-function generateOrder(listOfIngridients) {
+function generateOrder() {
+    $('#display_area').html(``)
     var arrays = [];
     this.listOfIngridients.forEach(element => {
         if(element.isAdded) {
            var finalIngridient = new Object();
            finalIngridient.name = element.ingridientName;
            arrays.push(finalIngridient.name);
-           console.log(arrays);
         }else{
         }
     });
